01d491c7ef45bdd8545fa93d555420cbdc61f662
Gabrusenas_A <gabrusenas@outlook.com>
2019-03-08 12:55:58 +0300

    Добавлен js. Открытие окна pop-up. Добавлены  яндекс карты

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,8 +1,13 @@
 var link = document.querySelector(".write-us");
 var popup = document.querySelector(".pop-up-login");
-  
+var popupClose = document.querySelector(".pop-up-close-button");
+
 link.addEventListener("click",function(evt){
     evt.preventDefault();
     popup.classList.add("modal-show");
-    console.log("клик");
+    console.log(popup.classList);
+});
+popupClose.addEventListener("click",function(evt){
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
 });
